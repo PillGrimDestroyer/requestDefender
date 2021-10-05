1c9682bf559c3720ea7785c5a1c559da771a687f
Roman <megamega4@yandex.ru>
2021-10-05 17:33:16 +0600

    Добавил контроль конфигов через переменные докера

--- src/main/java/kz/hawk/requestdefender/config/DbConfig.java
+++ src/main/java/kz/hawk/requestdefender/config/DbConfig.java
@@ -3,28 +3,34 @@ package kz.hawk.requestdefender.config;
 import kz.greetgo.conf.hot.DefaultIntValue;
 import kz.greetgo.conf.hot.DefaultStrValue;
 import kz.greetgo.conf.hot.Description;
+import kz.greetgo.conf.hot.FirstReadEnv;
 
 @Description("Настройки для управлением подключением к БД")
 public interface DbConfig {
 
   @Description("Хост postgres")
   @DefaultStrValue("localhost")
+  @FirstReadEnv("DB_HOST")
   String host();
 
   @Description("Порт postgres")
   @DefaultIntValue(13432)
+  @FirstReadEnv("DB_PORT")
   int port();
 
   @Description("Имя БД postgres")
   @DefaultStrValue("request_defender")
+  @FirstReadEnv("DB_NAME")
   String dbName();
 
   @Description("Имя пользователя в БД для доступа к нему")
   @DefaultStrValue("request_defender")
+  @FirstReadEnv("DB_USER")
   String username();
 
   @Description("Пароль доступа к БД")
   @DefaultStrValue("111")
+  @FirstReadEnv("DB_PASS")
   String password();
 
 }
--- src/main/java/kz/hawk/requestdefender/config/SecretRegisterConfig.java
+++ src/main/java/kz/hawk/requestdefender/config/SecretRegisterConfig.java
@@ -2,12 +2,14 @@ package kz.hawk.requestdefender.config;
 
 import kz.greetgo.conf.hot.DefaultIntValue;
 import kz.greetgo.conf.hot.Description;
+import kz.greetgo.conf.hot.FirstReadEnv;
 
 @Description("Настройки для управления секретами")
 public interface SecretRegisterConfig {
 
   @Description("Максимальная продолжительность хранения секретов. Указывается в днях")
   @DefaultIntValue(1)
+  @FirstReadEnv("SECRET_DAYS")
   Integer storeDays();
 
 }
